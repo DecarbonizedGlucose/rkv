5c23684514cf6110b6d5d889eea0de6a61b5dbea
DecarbonizedGlucose <decglu001@gmail.com>
2026-01-25 22:47:41 +0800

    fix: update protobuf and make the gen proto script can run anywhere

--- scripts/gen_rpc_proto.sh
+++ scripts/gen_rpc_proto.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-PROJECT_ROOT="$PWD/.."
-cd $PROJECT_ROOT
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
+cd "$PROJECT_ROOT"
 
 # generate raftrpc pb files
 protoc \
